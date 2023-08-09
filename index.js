b2c0dcf70c8378b5e2766be6c7b3687a2562ea53
Athenkosi Magada <magadaathenkosi@gmail.com>
2023-08-09 06:37:07 +0200

    Include database implementation

--- index.js
+++ index.js
@@ -1,97 +1,158 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fs from "fs";
+import mongoose from 'mongoose';
+import _ from 'lodash';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let todayTasks = [];
 
-fs.readFile("./today.txt", "utf8", (err, data) => {
-  if(err) throw err;
-  const dataArray = data.split('\n');
-
-  // Remove any empty lines from the array (optional).
-  const filteredArray = dataArray.filter((line) => line.trim() !== '');
-  filteredArray.forEach(task => {
-    const newTask = {
-      title: task,
-    };
-    todayTasks.push(newTask);
-  });
-  // Now you have the data from each line stored in the 'filteredArray'.
-  console.log(filteredArray);
-  console.log(todayTasks);
-
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-let workTasks = [];
-fs.readFile("./work.txt", "utf8", (err, data) => {
-  if(err) throw err;
-  const dataArray = data.split('\n');
-
-  const filteredArray = dataArray.filter((line) => line.trim() !== '');
-
-  filteredArray.forEach(task => {
-    const newTask = {
-      title: task,
-    };
-    workTasks.push(newTask);
-  });
-  // Now you have the data from each line stored in the 'filteredArray'.
-  console.log(filteredArray);
-  console.log(workTasks);
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please check your data entry']
+  },
 });
 
-app.get("/", (req, res) => {
-  const date = new Date();
-  var data;
-  let day = date.getDay();
-  day = weekdays[day];
+const Item = mongoose.model('Item', itemsSchema);
 
-  let month = date.getMonth();
-  month = months[month];
-  const todayDate = date.getDate();
-
-  data = day + ", " + month + " " + todayDate;
-
-  res.render("index.ejs",{ data, todayTasks });
+const item1 = new Item ({
+  name: 'Study Math',
+});
+const item2 = new Item ({
+  name: 'Cook food',
+});
+const item3 = new Item ({
+  name: 'Make cake',
 });
 
-app.get("/work", (req, res) => {
-  res.render("work.ejs",{workTasks});
-});
+const defaultItems = [item1, item2, item3];
 
-app.post("/submit", (req, res) => {
-  const newTask = {
-    title: req.body["title"],
+
+async function retrieveItems() {
+  try {
+    const items = await Item.find({});
+    if(items.length == 0) {
+      Item.insertMany(defaultItems)
+      .then(result => {
+        console.log("Save Items!");
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+    return items;
+  } catch (err) {
+    console.log(err);
   }
+}
 
-  fs.appendFile("today.txt", newTask.title + '\n', (err) => {
-    if(err) throw err;
-    console.log("Saved");
-  });
 
-  todayTasks.push(newTask);
-  res.redirect('/');
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model('List', listSchema);
+
+
+
+app.get("/", async (req, res) => {
+  const todayTasks = await retrieveItems();
+  res.render("list",{ listName: "Today", tasks: todayTasks });
 });
 
-app.post("/check", (req, res) => {
-  const newTask = {
-    title: req.body["title"],
-  };
+app.get("/:customListName", async (req, res) => {
+  const customListName = _.capitalize(req.params.customListName);
 
-  fs.appendFile("work.txt", newTask.title + '\n', (err) => {
-    if(err) throw err;
-    console.log("Saved");
+  try {
+    const list = await List.findOne({ name: customListName }).exec();
+    if (list) {
+      res.render("list", { listName: list.name, tasks: list.items });
+    } else {
+      const newList = new List({
+        name: customListName,
+        items: defaultItems,
+      });
+      await newList.save();
+      res.redirect("/" + customListName);
+    }
+  } catch (error) {
+    console.log("Error:", error);
+  }
+});
+
+
+app.post("/", async (req, res) => {
+  const itemName = req.body["title"];
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName,
   });
 
-  workTasks.push(newTask);
-  res.redirect('/work');
+  if (listName === "Today") {
+    await item.save();
+    res.redirect("/");
+  } else {
+    try {
+      const foundList = await List.findOne({ name: listName }).exec();
+      if (foundList) {
+        foundList.items.push(item);
+        await foundList.save();
+        res.redirect("/" + listName);
+      }
+    } catch (err) {
+      console.log("Error:", err);
+      res.redirect("/" + listName);
+    }
+  }
+});
+
+
+app.post("/delete", async (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+  console.log(checkedItemId);
+
+  if(listName === "Today"){
+    try {
+      await Item.findByIdAndRemove(checkedItemId);
+      console.log("Deleted item with ID:", checkedItemId);
+    res.redirect("/");
+    } catch (err) {
+      console.log("Error deleting item:", err);
+      res.redirect("/");
+    }
+  } else {
+    try {
+      const updatedList = await List.findOneAndUpdate(
+        { name: listName },
+        { $pull: { items: { _id: checkedItemId } } },
+        { new: true }
+      );
+      
+      console.log(updatedList);
+      if (updatedList) {
+        console.log("Removed item from custom list:", checkedItemId);
+        res.redirect("/" + listName);
+      } else {
+        console.log("Custom list not found");
+        res.redirect("/"  + listName);
+      }
+    } catch (err) {
+      console.log("Error removing item from custom list:", err);
+      res.redirect("/" + listName);
+    }
+  }
 });
 
 app.listen(port, () => {
